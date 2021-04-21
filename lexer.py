f8adf56117fbb538e8aab7a9203f7f8b43873b80
Electronick79 <63924606+Electronick79@users.noreply.github.com>
2021-04-20 21:24:10 -0400

    Initial commit

--- lexer.py
+++ lexer.py
@@ -1,54 +1,50 @@
 # Nickson Dos Santos
 # Comp-340 homework_5
-
+# https://github.com/Electronick79/Comp_340.git
 import string
 
-
-def tokenize(string):  # method tokenize
-    lis = []  # empty list to return
+lis = list()
 
 
-for i in range(0, len(string)):  # for every char in the string
-    dic = {}  # empty dictionary
-if string[i] == "(":  # if char is ( store corresponding type,value in dictionary
-    dic["type"] = "LPAREN"
-dic["value"] = string[i]  # here the dic looks like { "type" : "LPAREN", "value":"("}
-elif string[i] == ")":
-dic["type"] = "RPAREN"
-dic["value"] = string[i]
-elif string[i].isdigit():  # if the char is a digit
-if string[i - 1].isdigit():  # if the previous char is a digit continue to next position
-    continue
+class lexer:
 
-val = ""
-while string[i].isdigit():  # while the string[i] is digit add the string[i] to value
-    val = val + string[i]
-i += 1
-dic["type"] = "NUMBER"
-dic["value"] = val
-
-elif string[i] == "+":  # checking the char type and adding the type and value to the dictionary
-dic["type"] = "PLUS"
-dic["value"] = string[i]
-elif string[i] == "-":
-dic["type"] = "MINUS"
-dic["value"] = string[i]
-elif string[i] == "*":
-dic["type"] = "MULTIPLICATION"
-dic["value"] = string[i]
-elif string[i] == "/":
-dic["type"] = "DIVISION"
-dic["value"] = string[i]
-
-dic = dotdict(dic)  # making the dictionary keys,values to be able to access by the Dot(.) operator
-lis.append(dic)  # add the dictionary to the list
-
-return lis  # return the list
+    def __init__(self, type, value, ls=None):
+        self.type = type
+        self.value = value
+        lis.append(self)
 
 
-class dotdict(dict):  # class to make the dictionary values to be able to accessed by DOT(.) operator
-    __getattr__ = dict.get
+class lexer(lexer):
 
+    def tonkenize(srcCode, ls=None):
+        for scr in srcCode:
+            if scr == '(':
+                type = "LPAREN"
+                value = "("
 
+            elif scr == ')':
+                type = "RPAREN"
+                value = ")"
 
+            elif scr == '/':
+                type = "DIVISION"
+                value = "/"
 
+            elif scr == '*':
+                type = "MULTIPLICATION"
+                value = "*"
+
+            elif scr == '+':
+                type = "PLUS"
+                value = "+"
+
+            elif scr == '-':
+                type = "MINUS"
+                value = "-"
+
+            elif scr.isnumeric():
+                type = "NUMBERS"
+                value = str(scr)
+
+            lexer(type, value)
+        return lis
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 # Nickson Dos Santos
 # Comp-340 homework_5
+#https://github.com/Electronick79/Comp_340.git
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(lexer):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {lexer}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
 import lexer
 
 srcCode = "((12+3*5)+5/4)"
@@ -7,3 +27,4 @@ tokSeq = lexer.tokenize(srcCode)
 
 for i in tokSeq:
     print(i.type, i.value)
+
